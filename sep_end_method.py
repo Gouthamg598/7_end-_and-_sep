f80696055f885c25f09a5978fa7bc034c95bf31b
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-19 19:08:04 +0530

    Add files via upload

---
+++ sep_end_method.py
@@ -0,0 +1,31 @@
+'''seperation can only done by using sep'''
+# print(1,2,3,4,5,sep='-')
+# print('hello','my name is goutham','and my roll no is',41852,sep='')
+# print('python','3.4.10',sep='-')
+  
+'''use of end'''
+# print('hi\n'*3,end='')
+# print('my name is')#hihihimy name is
+# print('goutham')#goutham
+'''using end ="" method we will continue next print statement in same line '''
+# print('hi ',end='')
+# print('my name is ',end='')
+# print('goutham')
+'''combination of end and sep'''
+# print(12,'numeric',4,34,sep='-',end=',')
+# print(0,7,9,5,5,end=',')
+# print(78,'sum',45,4,4,64)
+
+
+# print(12,'number',4,34,sep='-',end=',')
+# print('zero',7,8,5,5,sep='-',end=',')
+# print(78,65,8,4,4,64,sep='-')
+# print('hi',end='.')
+# print('hey',end='-')
+# print('hello')
+# print('hello!',end='.')
+# print('how are',end=' ')
+# print('you?')
+# print('hello','py'  'thon',sep='-',end='.')#hello-python 
+#in this statement we are not used ,(cama) between 'py' 'thon' that is the reason they didn't get seperated with the (-)
+# print('python was developed by'+' '+'guido','van','rossum',sep='-',end='.')#pyhton was develped by guido-van-rossum
