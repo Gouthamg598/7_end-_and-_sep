d96d042dc95e974670851a4368758fd43d66ea9b
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-08-02 16:37:40 +0530

    Update sep_end_method.py

--- sep_end_method.py
+++ sep_end_method.py
@@ -10,22 +10,23 @@
 '''using end ="" method we will continue next print statement in same line '''
 # print('hi ',end='')
 # print('my name is ',end='')
-# print('goutham')
+# print('goutham')  #hi my name is goutham
 '''combination of end and sep'''
 # print(12,'numeric',4,34,sep='-',end=',')
 # print(0,7,9,5,5,end=',')
-# print(78,'sum',45,4,4,64)
-
-
+# print(78,'sum',45,4,4,64) #12-numeric-4-34,0 7 9 5 5,78 sum 45 4 4 64
 # print(12,'number',4,34,sep='-',end=',')
 # print('zero',7,8,5,5,sep='-',end=',')
 # print(78,65,8,4,4,64,sep='-')
+
 # print('hi',end='.')
 # print('hey',end='-')
-# print('hello')
+# print('hello')#hi.hey-hello
+
 # print('hello!',end='.')
 # print('how are',end=' ')
-# print('you?')
+# print('you?') #hello!.how are you?
+
 # print('hello','py'  'thon',sep='-',end='.')#hello-python 
-#in this statement we are not used ,(cama) between 'py' 'thon' that is the reason they didn't get seperated with the (-)
+'''in this statement we are not used ,(cama) between 'py' 'thon' that is the reason they didn't get seperated with the (-)'''
 # print('python was developed by'+' '+'guido','van','rossum',sep='-',end='.')#pyhton was develped by guido-van-rossum
